91380bc49b893ca9e81e30fd503582b05277d9e4
Roma Pushka <wwwwwanticrist@gmail.com>
2025-06-16 15:55:28 +0000

    Лабораторна 6. Створення репозиторія та оптимізація запитів

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -6,15 +6,24 @@ use App\Models\BlogCategory;
 use Illuminate\Support\Str;
 use App\Http\Requests\BlogCategoryUpdateRequest;
 use App\Http\Requests\BlogCategoryCreateRequest;
+use App\Repositories\BlogCategoryRepository;
 
 class CategoryController extends BaseController
 {
+    private $blogCategoryRepository;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->blogCategoryRepository = app(BlogCategoryRepository::class);
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        $paginator = BlogCategory::paginate(5);
+        $paginator = $this->blogCategoryRepository->getAllWithPaginate(5);
         return view('blog.admin.categories.index', compact('paginator'));
     }
 
@@ -25,7 +34,7 @@ class CategoryController extends BaseController
     public function create()
     {
          $item = new BlogCategory();
-            $categoryList = BlogCategory::all();
+            $categoryList = $this->blogCategoryRepository->getForComboBox();
 
             return view('blog.admin.categories.edit', compact('item', 'categoryList'));
     }
@@ -66,10 +75,14 @@ class CategoryController extends BaseController
      */
     public function edit($id)
     {
-        $item = BlogCategory::findOrFail($id);
-        $categoryList = BlogCategory::all();
+        $item = $this->blogCategoryRepository->getEdit($id);
+            if (empty($item)) {
+                abort(404);
+            }
 
-        return view('blog.admin.categories.edit', compact('item', 'categoryList'));
+            $categoryList = $this->blogCategoryRepository->getForComboBox();
+
+            return view('blog.admin.categories.edit', compact('item', 'categoryList'));
     }
 
 
@@ -78,7 +91,7 @@ class CategoryController extends BaseController
      */
     public function update(BlogCategoryUpdateRequest $request, $id)
     {
-        $item = BlogCategory::find($id);
+        $item = $this->blogCategoryRepository->getEdit($id);
         if (empty($item)) {
             return back()
                 ->withErrors(['msg' => "Запис id=[{$id}] не знайдено"])
---
+++ app/Repositories/BlogCategoryRepository.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\BlogCategory as Model;
+use Illuminate\Database\Eloquent\Collection;
+
+/**
+ * Class BlogСategoryRepository.
+ */
+class BlogCategoryRepository extends CoreRepository
+{
+    protected function getModelClass()
+    {
+        return Model::class; //абстрагування моделі BlogCategory, для легшого створення іншого репозиторія
+    }
+    /**
+     *  Отримати модель для редагування в адмінці
+     *  @param int $id
+     *  @return Model
+     */
+    public function getEdit($id)
+    {
+        return $this->startConditions()->find($id);
+    }
+
+    /**
+     *  Отримати список категорій для виводу в випадаючий список
+     *  @return Collection
+     */
+    public function getForComboBox()
+    {
+        //return $this->startConditions()->all();
+        $columns = implode(', ', [
+            'id',
+            'CONCAT (id, ". ", title) AS id_title',
+        ]);
+
+        $result = $this
+            ->startConditions()
+            ->selectRaw($columns)
+            ->toBase()
+            ->get();
+
+        return $result;
+
+    }
+
+    /**
+     * Отримати категорії для виводу пагінатором
+     *
+     * @param int|null $perPage
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function getAllWithPaginate($perPage = null)
+    {
+        $columns = ['id', 'title', 'parent_id'];
+
+        return $this
+            ->startConditions()
+            ->select($columns)
+            ->paginate($perPage);
+    }
+
+}
